c493c94892a8da808c92805673bd2598ed75f949
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-08-07 00:30:26 -0700

    sample data

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const messages = [
+  {
+    text: "Hello there!",
+    user: "Amanda",
+    added: new Date(),
+  },
+  {
+    text: "What's up!",
+    user: "Kevin",
+    added: new Date(),
+  },
+];
