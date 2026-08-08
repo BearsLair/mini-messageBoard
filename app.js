ad58f831dbe80d19c20d8f4581464522aa474498
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-08-07 17:42:12 -0700

    imported indexRouter

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const indexRouter = require("./routes/indexRouter");
+
+const path = require("node:path");
+const { runInThisContext } = require("node:vm");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(`Listening on port ${PORT}`);
+});
