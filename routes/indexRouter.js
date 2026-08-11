72c107fe5f5d7e0b9d70ba82b172984d00bd6c45
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-08-10 17:10:01 -0700

    Feat: View individual message

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,11 +4,13 @@ const indexRouter = Router();
 
 const messages = [
   {
+    id: 1,
     text: "Hello there!",
     user: "Amanda",
     added: new Date(),
   },
   {
+    id: 2,
     text: "What's up!",
     user: "Kevin",
     added: new Date(),
@@ -19,12 +21,24 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+indexRouter.get("/message/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.render("message", { message: messages[id - 1] });
+});
+
 indexRouter.get("/new", (req, res) => {
   res.render("form");
 });
 
 indexRouter.post("/new", (req, res) => {
-  console.log("Submitted!");
+  messages.push({
+    id: messages.length + 1,
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = indexRouter;
