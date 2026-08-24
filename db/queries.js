86fec74044cf2e8c26852557b2ca1f862b53928a
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-08-24 15:19:46 -0700

    created db pool, all/insert queries

---
+++ db/queries.js
@@ -0,0 +1,26 @@
+const pool = require("./pool");
+
+async function getAllMessages() {
+  try {
+    const { rows } = await pool.query("SELECT * FROM messages");
+    return rows;
+  } catch (error) {
+    console.error("db retrieval error: ", error);
+  }
+}
+
+async function insertMessage(username, message) {
+  try {
+    await pool.query(
+      "INSERT INTO messages (username, message) VALUES (($1), ($2))",
+      [username, message],
+    );
+  } catch (error) {
+    console.error("db insert error: ", error);
+  }
+}
+
+module.exports = {
+  getAllMessages,
+  insertMessage,
+};
